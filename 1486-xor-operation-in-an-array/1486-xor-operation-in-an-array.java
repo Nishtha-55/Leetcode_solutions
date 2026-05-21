8c23fc5f26a52efdaf252cc69e760d9c9617ae3b
Nishtha Srivastava <nishthasri098@gmail.com>
2026-05-21 23:13:20 +0530

    Time: 0 ms (100%), Space: 41.9 MB (73.64%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -3,21 +3,19 @@ class Solution {
         int nums[]=new int[n];
         int i=0;
         int c=n;
+int result=0;
         while(c!=0)
         {
             nums[i]=start + 2 * i ;
+             result=result^nums[i];
             i++;
             c--;
         }
-        /*for(int i=start;i<n;i++)
-        {
-            nums[i]=start + 2 * i ;
-        }*/
-         int result=0;
+       /*  int result=0;
         for(int j=0;j<n;j++)
         {
             result=result^nums[j];
-        }
+        }*/
         return result;
     }
 }
