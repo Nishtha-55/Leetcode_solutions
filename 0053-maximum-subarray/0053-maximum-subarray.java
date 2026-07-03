c28928ad3da19b0cd7de30744bc462f26deda3f4
Nishtha Srivastava <nishthasri098@gmail.com>
2026-07-03 23:44:35 +0530

    Time: 217 ms (1.64%), Space: 80.7 MB (7.77%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //kadane algo
+        int max=Integer.MIN_VALUE;
+        int curr=0;
+       for(int num:nums){
+        curr +=num;
+        if(curr>max)
+        max=curr;
+        if(curr<0)
+        curr=0;
+       }
+      return max;
+    }
+}
