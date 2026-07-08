5a01cfac0f4f1b1eac763b8f37d6e90d46febf7e
Nishtha Srivastava <nishthasri098@gmail.com>
2026-07-08 22:48:47 +0530

    Time: 0 ms (100%), Space: 42.1 MB (66.58%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,6 +1,28 @@
 class Solution {
     public int totalMoney(int n) {
-        int monday=1;
+         int terms = n / 7; // Weeks
+
+        int first = 28;
+        int last = 28 + (terms - 1) * 7; // AP formula for n-th term
+
+        int result = terms * (first + last) / 2; // Sum of nth terms in an AP
+
+        // Final week remaining days = n % 7
+        int startMoney = 1 + terms;
+
+        for (int day = 1; day <= (n % 7); day++) {
+            result += startMoney;
+            startMoney++;
+        }
+
+        return result;
+    }
+}
+
+//APPROACH 1 -> SIMULATION
+//T.C : O(n)
+//S.C : O(1)
+/*  int monday=1;
        
         int ans=0;
         while(n>0){
@@ -12,6 +34,8 @@ class Solution {
           n=n-7;
           monday++;
         }
-        return ans;
-    }
-}
+        return ans;*/
+
+//APPROACH 2 -> ARITHMETIC PROGRESSION
+/* 
+*/
