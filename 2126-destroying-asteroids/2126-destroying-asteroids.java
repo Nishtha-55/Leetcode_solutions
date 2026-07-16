3551f0e43e62a1bfec37ac048e9900e78364049c
Nishtha Srivastava <nishthasri098@gmail.com>
2026-07-16 16:03:35 +0530

    Time: 27 ms (19.25%), Space: 107.4 MB (50.99%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
-        int m=mass; //to avoid problem dur to large mass
+        long m=mass; //to avoid problem dur to large mass
         for(int ast:asteroids){
             if(m<ast)
            return false;
-           else if(m>Math.pow(10,5))
-           return true;
            m +=ast; 
            }
         return true;
@@ -14,3 +12,5 @@ class Solution {
 }
 
 //to avoid integer overflow 
+// else if(m>Math.pow(10,5))
+          // return true;
