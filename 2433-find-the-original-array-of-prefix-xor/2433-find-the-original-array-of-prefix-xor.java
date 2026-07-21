2e163bdef6f58e39104b795c65b39dc6227c6987
Nishtha Srivastava <nishthasri098@gmail.com>
2026-07-21 17:48:47 +0530

    Time: 2 ms (92.17%), Space: 109.2 MB (13.18%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.java
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.java
@@ -2,12 +2,20 @@ class Solution {
     public int[] findArray(int[] pref) {
         //logic pref[i]^pref[i-1]=ans[i]
         //property used - xor operation , cummutative and associative
-        int ans[]=new int[pref.length];
+      int n=pref.length;
+      for(int i=n-1;i>0;i--){
+        pref[i]=pref[i]^pref[i-1];
+      }
+      return pref;
+    }
+}
+
+
+//ELABORATED SOLUTION
+  /*int ans[]=new int[pref.length];
         //prefix of zero to wahi hoga 
         ans[0]=pref[0];
         for(int i=1;i<pref.length;i++){
             ans[i]=pref[i]^pref[i-1];
         }
-return ans;
-    }
-}
+return ans;*/
