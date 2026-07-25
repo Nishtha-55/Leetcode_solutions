5af51e4ca247c0752ea746b111ce549c23ce25d3
Nishtha Srivastava <nishthasri098@gmail.com>
2026-07-25 23:06:15 +0530

    Time: 212 ms (84.96%), Space: 46.9 MB (81.95%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,43 +1,34 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-         int n = nums.length;
-        
-        if(n < 3) {
-            HashSet<Integer> set = new HashSet<>();
-
-            for(int num : nums) {
-                set.add(num);
-            }
-
-            return set.size();
+       int n = nums.length;
+        int maxEl = 0;
+        for(int num : nums) {
+            maxEl = Math.max(maxEl, num);
         }
-
-        boolean[] pairXor = new boolean[2048];
-
-        for(int i=0; i<n; i++) {
-            for(int j=i; j<n; j++) {
-                pairXor[nums[i] ^ nums[j]] = true;
+        int T = 1;
+        while(T <= maxEl) {
+            T <<= 1; //T = T*2
+        }
+        boolean[] s1 = new boolean[T]; //XOR pair values set to true
+        boolean[] s2 = new boolean[T]; //XOR triplet values set to true
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                s1[nums[i] ^ nums[j]] = true;
             }
         }
-
-        boolean[] ans = new boolean[2048];
-
-        for(int i=0; i<2048; i++) {
-            if(!pairXor[i]) continue;
-
-            for(int num : nums) {
-                ans[i ^ num] = true;
+        for(int i = 0; i < T; i++) {
+            if(s1[i] == true) {
+                for(int num : nums) {
+                    s2[i ^ num] = true;
+                }
             }
         }
-
-        int count = 0;
-
-        for(boolean present : ans) {
-            if(present) count++;
+        int count = 0; //unique xor count
+        for(int i = 0; i < T; i++) {
+            if(s2[i] == true)
+                count++;
         }
-
         return count;
     }
 }
-
 // brute force se time limit exceed ho jayegi 
