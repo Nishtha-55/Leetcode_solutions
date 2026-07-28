7322e6c61944795fd94ebb2cc0a9dcdce3497790
Nishtha Srivastava <nishthasri098@gmail.com>
2026-07-28 23:16:53 +0530

    Time: 33 ms (43.78%), Space: 48.1 MB (75.9%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -1,12 +1,18 @@
 class Solution {
     public String smallestPalindrome(String s) {
+        //find length of array 
         int n=s.length();
+        //find mid 
         int mid=n/2;
+        //convert given string into character array for sorrting purpose 
         char c[]=s.toCharArray();
+//sort the first half of the array lexicographically 
         Arrays.sort(c,0,mid);
+    //now mirror the sorted part to make it palindrome
         for(int i=0;i<n;i++){
             c[n-1-i]=c[i];
         }
+        //convert array back to string and return 
         return String.valueOf(c);
     }
 }
