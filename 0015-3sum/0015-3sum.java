64a434342e0efc7e5d0e75386676cefd305d0387
Nishtha Srivastava <nishthasri098@gmail.com>
2026-08-26 22:39:37 +0530

    Time: 27 ms (98.82%), Space: 59.2 MB (45.29%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,6 +1,71 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
+        if (nums.length < 3) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            twoSum(nums, i + 1, result, -nums[i]);
+        }
+
+        return result;
+    }
+
+    private void twoSum(int[] nums, int k, List<List<Integer>> result, int target) {
+        int i = k, j = nums.length - 1;
+
+        while (i < j) {
+            if (nums[i] + nums[j] > target) {
+                j--;
+            } else if (nums[i] + nums[j] < target) {
+                i++;
+            } else {
+                result.add(Arrays.asList(-target, nums[i], nums[j]));
+
+                while (i < j && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+                while (i < j && nums[j] == nums[j - 1]) {
+                    j--;
+                }
+
+                i++;
+                j--;
+            }
+        }
+    }
+}
+// APPROACH 1 - brute force
+/* 
+ int n=nums.length;
+        Set<List<Integer>> st=new HashSet<>();
+     for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                if(nums[i]+nums[j]+nums[k]==0)
+                {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(temp);
+                        st.add(temp);
+
+                }
+            }
+        }
+     }
+     return new ArrayList<>(st); */
+
+
+
+
+     //APPROACH 2 -- HASHING
+     /*
+      int n = nums.length;
         Set<List<Integer>> ans = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
@@ -22,23 +87,4 @@ class Solution {
         }
 
         return new ArrayList<>(ans);
-    }
-}
-// APPROACH 1 - brute force
-/* 
- int n=nums.length;
-        Set<List<Integer>> st=new HashSet<>();
-     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            for(int k=j+1;k<n;k++){
-                if(nums[i]+nums[j]+nums[k]==0)
-                {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
-                        Collections.sort(temp);
-                        st.add(temp);
-
-                }
-            }
-        }
-     }
-     return new ArrayList<>(st); */
+    */
